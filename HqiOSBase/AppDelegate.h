fef0108720ac7577f246b90e6608dfa18f3e14a5
hehuiqi <253956882@qq.com>
2018-03-10 14:25:16 +0800

    Initial Commit

---
+++ HqiOSBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HqiOSBase
+//
+//  Created by hehuiqi on 2018/3/10.
+//  Copyright © 2018年 hehuiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
